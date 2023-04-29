e686bd813a4758778ab421cf80af17631343942f
semestralnapraca <130559960+semestralnapraca@users.noreply.github.com>
2023-04-29 13:56:10 +0200

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -9,6 +9,38 @@ let closeVideo = document.querySelector('.close_video');
 let burgerLinks = document.querySelectorAll('.burger_links');
 const body = document.body;
 
+let tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+  videoId: 'GBj9bxHuuXE',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+let done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
+
 function close(){
     burgerBtn.classList.remove('open');
     burgerMenu.classList.remove('open');
@@ -49,35 +81,3 @@ burgerLinks.forEach((item) => {
         close()
     })
 })
-
-
-let tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    let firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    let player;
-    function onYouTubeIframeAPIReady() {
-      player = new YT.Player('player', {
-      videoId: 'GBj9bxHuuXE',
-        events: {
-          'onReady': onPlayerReady,
-          'onStateChange': onPlayerStateChange
-        }
-      });
-    }
-    function onPlayerReady(event) {
-      event.target.playVideo();
-    }
-
-    let done = false;
-    function onPlayerStateChange(event) {
-      if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-      }
-    }
-
-    function stopVideo() {
-      player.stopVideo();
-    }
